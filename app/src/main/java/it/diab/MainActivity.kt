9cd13cf3d04e36d8d6f8659c983e54f16aa1b170
Joey <bevilacquajoey@gmail.com>
2018-05-08 14:52:12 +0200

    Add "Add glucose" shortcut
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/diab/MainActivity.kt
+++ app/src/main/java/it/diab/MainActivity.kt
@@ -1,17 +1,28 @@
 package it.diab
 
+import android.annotation.TargetApi
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.support.annotation.DrawableRes
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.TabLayout
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.preference.PreferenceManager
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
@@ -51,6 +62,10 @@ class MainActivity : AppCompatActivity() {
         mViewPager.adapter = ViewPagerAdapter(supportFragmentManager)
         mTabLayout.setupWithViewPager(mViewPager)
         mFab.setOnClickListener(this::onFabClick)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            addShortcuts()
+        }
     }
 
     override fun onResume() {
@@ -95,6 +110,53 @@ class MainActivity : AppCompatActivity() {
         mOverviewFragment.update(data, mViewModel.getAverageLastWeek())
     }
 
+    @TargetApi(26)
+    private fun addShortcuts() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        if (prefs.getInt(KEY_SHORTCUTS, SHORTCUTS_VERSION - 1) >= SHORTCUTS_VERSION) {
+            return
+        }
+
+        val manager = getSystemService(ShortcutManager::class.java)
+
+        val title = getString(R.string.app_shortcut_add_glucose)
+        val editIntent = Intent(this, EditorActivity::class.java)
+        editIntent.putExtra(EditorActivity.EXTRA_INSERT_MODE, true)
+        editIntent.action = Intent.ACTION_VIEW
+        val bm = getShortcutIcon(R.drawable.ic_shortcut_add_glucose)
+        val addShortcut = ShortcutInfo.Builder(this, title)
+                .setShortLabel(title)
+                .setLongLabel(title)
+                .setIntent(editIntent)
+                .setIcon(Icon.createWithAdaptiveBitmap(bm))
+                .build()
+
+        manager.removeAllDynamicShortcuts()
+        manager.addDynamicShortcuts(listOf(addShortcut))
+
+        prefs.edit().putInt(KEY_SHORTCUTS, SHORTCUTS_VERSION).apply()
+    }
+
+    private fun getShortcutIcon(@DrawableRes icon: Int): Bitmap {
+        val drawable = ContextCompat.getDrawable(this, icon)
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+
+        if (drawable == null) {
+            throw IllegalArgumentException("Could not get a valid drawable from argument")
+        }
+
+        val bm = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bm)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+
+        return bm
+    }
+
+
     inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
         private val mFragments = arrayOf(mOverviewFragment, mGlucoseFragment)
 
@@ -102,5 +164,10 @@ class MainActivity : AppCompatActivity() {
         override fun getItem(position: Int) = mFragments[position]
         override fun getPageTitle(position: Int): String =
                 getString(mFragments[position].getTitle())
+    }
+
+    companion object {
+        private const val SHORTCUTS_VERSION = 0
+        private const val KEY_SHORTCUTS = "pref_home_shortcuts"
     }
 }
