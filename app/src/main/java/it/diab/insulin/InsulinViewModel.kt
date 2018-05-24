f1639a2935e0ff7923ca57a5e5aec8759bd8940d
Joey <bevilacquajoey@gmail.com>
2018-05-24 21:27:24 +0200

    Apply linter suggestions
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/diab/insulin/InsulinViewModel.kt
+++ app/src/main/java/it/diab/insulin/InsulinViewModel.kt
@@ -3,9 +3,7 @@ package it.diab.insulin
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
-
 import it.diab.db.AppDatabase
-import it.diab.db.DatabaseTask
 import it.diab.db.entities.Insulin
 
 class InsulinViewModel(owner: Application) : AndroidViewModel(owner) {
--- app/src/main/java/it/diab/util/extensions/Context.kt
+++ app/src/main/java/it/diab/util/extensions/Context.kt
@@ -1,11 +1,6 @@
 package it.diab.util.extensions
 
 import android.content.Context
-import android.graphics.Color
-import android.support.annotation.ColorInt
 import android.support.annotation.DimenRes
-import android.support.annotation.StyleableRes
-import android.util.AttributeSet
-import android.util.TypedValue
 
 fun Context.getDimen(@DimenRes dimenId: Int) = resources.getDimensionPixelSize(dimenId)
