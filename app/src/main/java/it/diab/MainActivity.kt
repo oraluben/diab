89494ade9a45d31d0bb86a8bc712435a12905df2
Joey <bevilacquajoey@gmail.com>
2018-07-24 15:47:02 +0200

    Remove unused preference libs
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/diab/MainActivity.kt
+++ app/src/main/java/it/diab/MainActivity.kt
@@ -14,6 +14,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.annotation.DrawableRes
 import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.FloatingActionButton
@@ -27,7 +28,6 @@ import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.preference.PreferenceManager
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
