eddd45a0ab62fb8e7d74a492d9a71f226960555f
Joey <bevilacquajoey@gmail.com>
2018-08-08 00:08:59 +0200

    Database: improve singletonHolder getInstance's logic
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/diab/util/SingletonHolder.kt
+++ app/src/main/java/it/diab/util/SingletonHolder.kt
@@ -1,25 +1,11 @@
 package it.diab.util
 
-open class SingletonHolder<out T, in A>(creator: (A) -> T) {
+abstract class SingletonHolder<out T, in A>(creator: (A) -> T) {
     private var creator: ((A) -> T)? = creator
     @Volatile private var instance: T? = null
 
-    fun getInstance(arg: A): T {
-        val i = instance
-        if (i != null) {
-            return i
-        }
-
-        return synchronized(this) {
-            val i2 = instance
-            if (i2 != null) {
-                i2
-            } else {
-                val created = creator!!(arg)
-                instance = created
-                creator = null
-                created
-            }
-        }
+    fun getInstance(arg: A) =
+        instance ?: synchronized(this) {
+            instance ?: creator!!(arg).also { instance = it }
     }
 }
