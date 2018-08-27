c34dcf3cb0b7610712db692d87f21c4c8e0e3cef
Joey <bevilacquajoey@gmail.com>
2018-08-27 20:41:25 +0200

    Diab: lint cleanup
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/diab/util/extensions/Context.kt
+++
@@ -1,6 +0,0 @@
-package it.diab.util.extensions
-
-import android.content.Context
-import androidx.annotation.DimenRes
-
-fun Context.getDimen(@DimenRes dimenId: Int) = resources.getDimensionPixelSize(dimenId)
