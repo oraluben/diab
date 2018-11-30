1a1559299e95bfb3be0bdf3b63e4651b41968c32
Joey <bevilacquajoey@gmail.com>
2018-11-30 17:47:36 +0100

    Insert license header into the files
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- app/src/main/java/it/diab/util/SingletonHolder.kt
+++ app/src/main/java/it/diab/util/SingletonHolder.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018 Bevilacqua Joey
+ *
+ * Licensed under the GNU GPLv3 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl.txt
+ */
 package it.diab.util
 
 abstract class SingletonHolder<out T, in A>(creator: (A) -> T) {
