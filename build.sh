f01bc4dbdbffc3e12a93cd685a355c7151f000c4
Joey <bevilacquajoey@gmail.com>
2019-01-10 22:35:53 +0100

    Remove bitbucket pipeline support
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir "${ANDROID_HOME}/licenses" || true
-echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "${ANDROID_HOME}/licenses/android-sdk-license"
-echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> "${ANDROID_HOME}/licenses/android-sdk-license"
-
-echo "diab.fit=false" >> ../local.properties
-
-../gradlew assembleDebug -PisCiBuild
